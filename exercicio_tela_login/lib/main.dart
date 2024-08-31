d9322cd9e4bc0257f333c6f62c98f7c193d996b1
luis_souz <souza_lipes@outlook.com>
2024-08-31 11:30:19 -0300

    FInalizacao exercicio tela login

--- exercicio_tela_login/lib/main.dart
+++ exercicio_tela_login/lib/main.dart
@@ -16,16 +16,50 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: const Text('Login', style: TextStyle(color: Colors.white),), centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-      body: Column(
+      drawer: const Drawer(
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+          decoration: const BoxDecoration(image: DecorationImage(
+            image: AssetImage('./images/back.webp'),
+            fit: BoxFit.cover
+          )),
+        child: Align( alignment: Alignment.center,
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
+            const SizedBox(
+              width: 200,
+              child: TextField(
+              decoration: InputDecoration(labelText: "Usuario"),
+              style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+            ),
+            const SizedBox(
+              width: 200,
+              child:            TextField(
+              decoration: InputDecoration(labelText: "Senha"),
+              style: TextStyle(fontSize: 15, color: Colors.black),
+            ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: (){}, child: const Text("Entrar"))
           ],
         ),
+      ),
+      ),
+      bottomNavigationBar:  BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil"),
+      ]),
       ),
     );
   }
